64baa5a40b81d56e84661f52881f3f0b9cb03296
filipesoaresdev <filipe.shaulin@gmail.com>
2019-10-17 15:17:30 -0300

    (Adding comment)Design Pattern - Factpry Method. Explanation about the factory method pattern.

--- src/main/java/com/futureday/java_design_patterns/factorymethod/Client.java
+++ src/main/java/com/futureday/java_design_patterns/factorymethod/Client.java
@@ -8,8 +8,23 @@ package com.futureday.java_design_patterns.factorymethod;
 import com.futureday.java_design_patterns.factorymethod.message.Message;
 
 /**
+ * This client demonstrate the use of an Factory Method. We are going to 
+ * send a message which can be a JSON message or a Text message.
+ * 
+ * 1 - Firstly, is created an abstract class called MessageCreator. This class has
+ * the factory method (abstract) called createMessage() and a public method to 
+ * get the message.
+ * 2 - We have two classes of Message (JSONMessage and TextMessage) which 
+ * extend from Message;
+ * 3 - So, we can have multiple classes implementing MessageCreator. In this 
+ * case they are JSONMessageCreater and TextMessageCreater. These classes 
+ * implement the createMessage() returning the class of the respective messages.
+ * 4 - This client print each message calling a unique method 
+ * printMessage(MessageCreator messageCreator) that call the getMessage() 
+ * method. The type of message creator is determined when calling this method, 
+ * passing as parameter the specific subclass of MessageCreator
  *
- * @author FilipeSoares-STI
+ * @author FilipeSoares
  */
 public class Client {
     
